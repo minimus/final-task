3c2b7b825f7976eb90ae0b0380fcd8bc85dbb046
Konstantin Leonov <minimus@simplelib.com>
2017-05-20 20:24:03 +0300

    Some changes

--- client-src/components/pager/PagerButtonsOnePane.js
+++ client-src/components/pager/PagerButtonsOnePane.js
@@ -5,7 +5,10 @@ import PagerButton from './PagerButton'
 export default function PagerButtonsOnePane({ base, category, pages }) {
   const pagesArray = []
   let i = 1
-  while (i <= pages) pagesArray.push(i += 1)
+  while (i <= pages) {
+    pagesArray.push(i)
+    i += 1
+  }
   return (
     <span id="pager-buttons-pane">
       {pagesArray.map(idx => (
